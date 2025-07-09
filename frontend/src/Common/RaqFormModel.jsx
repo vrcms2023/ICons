1f021567102495915c90079fa2120aac4bb0b09f
unknown <prasadnti@gmail.com>
2025-07-09 09:52:13 +0530

    Home Service Changes

--- frontend/src/Common/RaqFormModel.jsx
+++ frontend/src/Common/RaqFormModel.jsx
@@ -8,7 +8,7 @@ const RaqFormModel = ({ closeModel, downloadPDF }) => {
       <div className="modal-dialog modal-dialog-centered">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title text-dark fw-bold">RAQ Form</h5>
+            <h5 className="modal-title text-dark fw-bold">Request For Quotation</h5>
             <button
               type="button"
               className="btn-close"
