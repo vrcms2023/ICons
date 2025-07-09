78b51249ad807b96a92c031b60574e9e4e86df27
unknown <prasadnti@gmail.com>
2025-07-09 22:12:27 +0530

    Client specific UI Changes

--- frontend/src/Common/StyledComponents/Styled-Clients.js
+++ frontend/src/Common/StyledComponents/Styled-Clients.js
@@ -4,10 +4,13 @@ export const ClientStyled = styled.div`
   // .clients hr:last-child {
   //   display: none;
   // }
-  .clientAvatar img {
-    width: 100px;
-    height: 100px;
-    object-fit: contain;
+  .clientAvatar{
+
+     img {
+      width: auto;
+      height: 100px;
+      object-fit: contain;
+    }
   }
  
   .clientFrontend  {
@@ -19,12 +22,12 @@ export const ClientStyled = styled.div`
 
     &.overlayContainer {
       //background: ${({theme}) => theme.primaryColor};
-      border: 1px solid ${({theme}) => theme.gray999};
+      border: 1px solid ${({theme}) => theme.grayddd};
       border-radius: 2px;
       position: relative;
       // width: 50%;
       // max-width: 300px;
-      min-height: 200px;
+      // min-height: 200px;
     }
     
     /* Make the image to responsive */
@@ -34,55 +37,55 @@ export const ClientStyled = styled.div`
       // height: auto;
     }
   
-    .overlay {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      background: rgb(0, 0, 0);
-      background: rgba(0, 0, 0, .8); /* Black see-through */
-      color: #f1f1f1;
-      width: 100%;
-      transition: .5s ease;
-      opacity:0;
-      color: white;
-      font-size: 20px;
-      padding: 20px;
-      // border-radius: 10px;
-      height: 100%;
-      max-height: 250px;
-      overflow-y: auto;
-      visibility: visible !important;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
+    // .overlay {
+    //   position: absolute;
+    //   bottom: 0;
+    //   left: 0;
+    //   background: rgb(0, 0, 0);
+    //   background: rgba(0, 0, 0, .8); /* Black see-through */
+    //   color: #f1f1f1;
+    //   width: 100%;
+    //   transition: .5s ease;
+    //   opacity:0;
+    //   color: white;
+    //   font-size: 20px;
+    //   padding: 20px;
+    //   // border-radius: 10px;
+    //   height: 100%;
+    //   max-height: 250px;
+    //   overflow-y: auto;
+    //   visibility: visible !important;
+    //   display: flex;
+    //   flex-direction: column;
+    //   justify-content: center;
+    //   align-items: center;
 
 
-      &::before {
-        font-size: 3rem;
-        position: sticky;
-        right: 15px !important;
-        top: -10px;
-        color: rgba(255, 165, 0, .8);  
-        display: block;
-        width: 100%;  
-      }
+    //   &::before {
+    //     font-size: 3rem;
+    //     position: sticky;
+    //     right: 15px !important;
+    //     top: -10px;
+    //     color: rgba(255, 165, 0, .8);  
+    //     display: block;
+    //     width: 100%;  
+    //   }
 
-      &::-webkit-scrollbar {
-        width: 8px;
-      }
+    //   &::-webkit-scrollbar {
+    //     width: 8px;
+    //   }
       
-      &::-webkit-scrollbar-track {
-          -webkit-box-shadow: inset 0 0 6px rgba(225,242,253,0.3); 
-          border-radius: 3px;
-      }
+    //   &::-webkit-scrollbar-track {
+    //       -webkit-box-shadow: inset 0 0 6px rgba(225,242,253,0.3); 
+    //       border-radius: 3px;
+    //   }
       
-      &::-webkit-scrollbar-thumb {
-          border-radius: 10px;
-          -webkit-box-shadow: inset 0 0 6px rgba(232,252,187,0.5); 
-      }
+    //   &::-webkit-scrollbar-thumb {
+    //       border-radius: 10px;
+    //       -webkit-box-shadow: inset 0 0 6px rgba(232,252,187,0.5); 
+    //   }
       
-    }
+    // }
 
     p {
       font-size: .9rem;
@@ -101,5 +104,7 @@ export const ClientStyled = styled.div`
     gap: 15px;
     flex-wrap: wrap;
   }
-  
+  .quill .ql-editor {
+    padding: 0px;
+  }
 `;
