71319d2e05ab5b8a37d673c24e857f6823d0a6f4
unknown <prasadnti@gmail.com>
2025-07-10 21:06:00 +0530

    HomeServices and Keypoints UI changes

--- frontend/src/Common/StyledComponents/Styled-Keypoints.js
+++ frontend/src/Common/StyledComponents/Styled-Keypoints.js
@@ -4,15 +4,16 @@ export const KeypointsStyled = styled.div`
     
     .IconsMainKeys {
         padding: 16px 24px;
-        // background-color:rgb(141, 141, 141);
-        // background-color: ${({ theme }) => theme.gray333};
+        background-color:rgb(175, 201, 253);
+        // background-color: ${({ theme }) => theme.gray222};
     //    background: linear-gradient(163deg,rgba(109, 47, 155, 0.76) 0%, rgba(1, 32, 96, 0.7) 40%);
     //    background: linear-gradient(163deg,rgba(1, 32, 96, 0.80) 0%, rgba(109, 47, 155, 0.9) 50%);
-    background: linear-gradient(185deg,rgba(255, 255, 255, 0.8) 0%, rgba(1, 32, 96, 0.8) 30%);
-        color: ${({ theme }) => theme.white};
+    // background: linear-gradient(185deg,rgba(255, 255, 255, 0.8) 0%, rgba(1, 32, 96, 0.8) 30%);
+    // background: linear-gradient(163deg,rgba(122, 155, 222, 1) 0%, rgba(102, 120, 170, 0.66) 100%);
+        color: ${({ theme }) => theme.gray222};
         margin-bottom: 32px;
         border-radius: 8px;
-        // box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .10) !important;
+        box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .10) !important;
         
     }
 
@@ -32,7 +33,7 @@ export const KeypointsStyled = styled.div`
     }
 
     .point {
-        font-size: 1.3rem;
+        font-size: 1.2rem;
         font-weight: 400;
     }
 `;
