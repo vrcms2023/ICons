d0f5a574cc70546c630a9348fc69eff48f9aacb8
unknown <prasadnti@gmail.com>
2025-07-12 14:55:13 +0530

    General CSS Effects

--- frontend/src/Common/StyledComponents/Styled-ListAllServicesPage.js
+++ frontend/src/Common/StyledComponents/Styled-ListAllServicesPage.js
@@ -2,14 +2,30 @@ import styled from 'styled-components'
 
 export const ShowAllServicesPage = styled.div`
     padding: 48px 0 24px;
+    
     .allService {
         // box-shadow: 0 .3rem .7rem rgba(0, 0, 0, .15) !important;
+
+        transition: background-color 0.4s ease;
         .allServiceImg {
             text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background:rgb(248, 248, 248);
+            padding: 10px !important;
+
             img{
                 width: 100%;
-                height: 240px;
+                max-height: 100%;
                 object-fit: cover;
+                // border-radius: 8px;
+                transition: transform 0.4s ease;
+
+                &:hover {
+                    // box-shadow: 0 .3rem .7rem rgba(0, 0, 0, .25) !important;
+                    // border-radius: 8px;
+                }
             }
         }
 
@@ -39,6 +55,12 @@ export const ShowAllServicesPage = styled.div`
             a, a span {
                 font-size: .9rem !important;
             }
+        }
+
+        &:hover {background:rgb(248, 248, 248);}
+
+        &:hover .allServiceImg img {
+            transform: scale(1.1);
         }
     }
 
