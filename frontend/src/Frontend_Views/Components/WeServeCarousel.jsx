3a5ae63746d6b75407ae6b674f56c89554de85d7
Ramakrishna <ramajsworks@gmail.com>
2025-07-13 10:46:08 +0530

    update carouse changes

--- frontend/src/Frontend_Views/Components/WeServeCarousel.jsx
+++ frontend/src/Frontend_Views/Components/WeServeCarousel.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 
 // Components
 import { axiosClientServiceApi } from "../../util/axiosUtil";
@@ -13,24 +10,9 @@ import { getImagePath, sortByFieldName } from "../../util/commonUtil";
 import SkeletonImage from "../../Common/Skeltons/SkeletonImage";
 import { WeServeCarouselItem } from "./WeServeCarouselItem";
 
-
 const WeServeCarousel = ({ carouselState, category }) => {
   const { isLoading } = useSelector((state) => state.loader);
-  const [carousel, setCarousel] = useState([]);
-  const settings = {
-    className: "slider variable-width",
-    dots: false,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 3000,
-    variableWidth: true,
-    draggable: true,
-    swipeToSlide: true,
-    cssEase: "ease",
-  };
+  const [carouselList, setCarouselList] = useState([]);
 
   useEffect(() => {
     const getCarousels = async () => {
@@ -46,7 +28,7 @@ const WeServeCarousel = ({ carouselState, category }) => {
             "carouse_position"
           );
 
-          setCarousel(carouselList);
+          setCarouselList(carouselList);
         }
       } catch (error) {
         console.log("unable to access ulr because of server is down");
@@ -60,21 +42,17 @@ const WeServeCarousel = ({ carouselState, category }) => {
   return (
     <div className="">
       {isLoading ? <SkeletonImage /> : ""}
-        <div className="slider-container">
-          <Slider {...settings}>
-            {carousel.map((item, index) => {
-              return (
-                <WeServeCarouselItem key={item.id} item={item} index={index} />
-              );
-            })}
-          </Slider>
-        </div>
-
-        {carousel.length === 0 && (
-          <div className="d-flex justify-content-center align-items-center fs-5 text-muted text-center noImg">
-            {!isLoading && <p>Please add images for Carousel...</p>}
-          </div>
+      <div className="slider-container">
+        {carouselList.length > 0 && (
+          <WeServeCarouselItem carouselList={carouselList} />
         )}
+      </div>
+
+      {carouselList.length === 0 && (
+        <div className="d-flex justify-content-center align-items-center fs-5 text-muted text-center noImg">
+          {!isLoading && <p>Please add images for Carousel...</p>}
+        </div>
+      )}
     </div>
   );
 };
